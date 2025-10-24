6a0ad168257e0e11254fb54935de1fd21621cdb4
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-10-24 11:00:36 +0200

    I have started making the api calls through a custom hook

---
+++ src/components/AnimatedDetails/AnimatedDetails.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './AnimatedDetails.sass';
+import { IoChevronForwardOutline } from 'react-icons/io5';
+import Logo from '../assets/newsify-logo.svg';
+
+export default function AnimatedDetails({ title, children, ...props }) {
+	//Handle fold in fold out height
+	const handleToggle = (event) => {
+		const element = event.target;
+
+		const openHeight = element.scrollHeight + 'px';
+
+		element.style.height = element.open ? openHeight : null;
+	};
+
+	return (
+		<>
+			<details onToggle={handleToggle} className="animated-details" {...props}>
+				<summary className="animated-details__summary">
+					<div className="animated-details__summary-header">
+						<img
+							src={Logo}
+							alt="Newsify logo"
+							className="animated-details__logo"
+						/>
+						{title}{' '}
+					</div>
+					<IoChevronForwardOutline className="animated-details__arrow" />
+				</summary>
+				{children}
+			</details>
+		</>
+	);
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import Header from '../components/Header/Header';
 import SearchBar from '../components/SearchBar/SearchBar';
 import NavBar from '../components/NavBar/NavBar';
+import AnimatedDetails from '../components/AnimatedDetails/AnimatedDetails';
+import '../style/_layout.sass';
 
 export default function Home() {
 	useEffect(() => {
@@ -11,6 +13,39 @@ export default function Home() {
 		<section className="main-page">
 			<Header />
 			<SearchBar />
+			<AnimatedDetails title="Health" name="news">
+				<>
+					<h2>Health</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quam
+						possimus id molestias ipsa eaque blanditiis voluptatum quaerat,
+						voluptate porro delectus laudantium, magni, unde ipsam. Vero maiores
+						autem eaque magni.
+					</p>
+				</>
+			</AnimatedDetails>
+			<AnimatedDetails title="Sport" name="news">
+				<>
+					<h1>Sport</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quam
+						possimus id molestias ipsa eaque blanditiis voluptatum quaerat,
+						voluptate porro delectus laudantium, magni, unde ipsam. Vero maiores
+						autem eaque magni.
+					</p>
+				</>
+			</AnimatedDetails>
+			<AnimatedDetails title="Travel" name="news">
+				<>
+					<h1>Travel</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quam
+						possimus id molestias ipsa eaque blanditiis voluptatum quaerat,
+						voluptate porro delectus laudantium, magni, unde ipsam. Vero maiores
+						autem eaque magni.
+					</p>
+				</>
+			</AnimatedDetails>
 			<NavBar />
 		</section>
 	);
