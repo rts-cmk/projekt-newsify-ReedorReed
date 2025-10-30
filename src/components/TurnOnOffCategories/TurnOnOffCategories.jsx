fb79f173b9f96f09c5bb43706ae29739896e2907
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-10-30 13:28:01 +0100

    finished the settings page

---
+++ src/components/TurnOnOffCategories/TurnOnOffCategories.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { useCategories } from './CategoryContext';
+import Logo from '../assets/newsify-logo.svg';
+import './TurnOnOffCategories.sass';
+
+const TurnOnOffCategories = () => {
+	const { categories, toggleCategory } = useCategories();
+
+	const categoryList = [
+		{ key: 'home', label: 'Today' },
+		{ key: 'health', label: 'Health' },
+		{ key: 'food', label: 'Food' },
+		{ key: 'travel', label: 'Travel' }
+	];
+
+	return (
+		<>
+			<div className="category-settings">
+				{categoryList.map((category) => (
+					<div key={category.key} className="category-settings__toggle-item">
+						<figure className="category-settings__info">
+							<img src={Logo} className="category-settings__icon" />
+							<figcaption>
+								<h2 className="category-settings__label">{category.label}</h2>
+							</figcaption>
+						</figure>
+
+						<button
+							onClick={() => toggleCategory(category.key)}
+							className={`toggle-switch ${
+								categories[category.key] ? 'active' : ''
+							}`}>
+							<span className="toggle-slider" />
+						</button>
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default TurnOnOffCategories;
